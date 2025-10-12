6cde28c3c984776d13ad71e390ad50889f21430f
Shiyuan-Duan <shiyuanduan2023@u.northwestern.edu>
2025-10-12 13:17:29 -0500

    10/12 test

--- visualization.py
+++ visualization.py
@@ -277,16 +277,20 @@ def _convert_numpy_types(obj):
     """Recursively convert numpy types to native Python types for JSON serialization"""
     if isinstance(obj, dict):
         return {key: _convert_numpy_types(value) for key, value in obj.items()}
-    elif isinstance(obj, list):
+    elif isinstance(obj, (list, tuple)):
         return [_convert_numpy_types(item) for item in obj]
-    elif isinstance(obj, (np.integer, np.int64, np.int32, np.int16, np.int8)):
-        return int(obj)
-    elif isinstance(obj, (np.floating, np.float64, np.float32, np.float16)):
-        return float(obj)
-    elif isinstance(obj, (np.bool_, bool)):
-        return bool(obj)
     elif isinstance(obj, np.ndarray):
         return obj.tolist()
+    elif isinstance(obj, np.integer):
+        return int(obj)
+    elif isinstance(obj, np.floating):
+        return float(obj)
+    elif isinstance(obj, np.bool_):
+        return bool(obj)
+    elif isinstance(obj, (np.int64, np.int32, np.int16, np.int8)):
+        return int(obj)
+    elif isinstance(obj, (np.float64, np.float32, np.float16)):
+        return float(obj)
     else:
         return obj
 
